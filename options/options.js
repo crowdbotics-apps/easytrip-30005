8b67e26aacf868a171c18c6139e7029397303e86
Crowdbotics <team@crowdbotics.com>
2021-08-21 10:43:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "easytrip_30005Identifier",
+  url: "https://easytrip_30005Identifier.botics.co",
+  api: "https://easytrip_30005Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
